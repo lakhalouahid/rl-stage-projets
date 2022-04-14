7ee3cffbfabf79d0fb662a7ccaf67be57c93859e
Ouahid LAK-HAL <ouahid.lak-hal@etu.uca.fr>
2022-04-14 17:29:24 +0200

    feat(blackjack): implementation insane fast with numba

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,25 +1,45 @@
 import numpy as np
-np.set_printoptions(linewidth=10000, precision=3)
+from numba import jit, boolean, int8, int32, int64
+from numba.experimental import jitclass
 
-usable_ace = True
-accuracy = 1e-4
-cards_space = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-cards_space = np.array(cards_space, dtype=np.int32)
+np.set_printoptions(linewidth=10000, precision=5)
 
 
-push_action, stick_action = 1, 0
-A = np.zeros((10, 10), dtype=np.int8)
-V = np.zeros((10, 10), dtype=np.float32)
-nV = np.ones((10, 10), dtype=np.int32)
-R = np.array([-1, 0, 1], dtype=np.int32)
 
-A[:8, :] = 1
-print(A)
+cards_space = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], dtype=np.int8)
+stick_action: int = 0
+push_action: int = 1
 
-def sample_card():
+@jit(nopython=True, cache=True)
+def sample_card() -> int:
   return np.random.choice(cards_space)
 
-def get_sum(cards):
+
+
+
+@jitclass([('count', int32), ('shoe', int8[:]), ('pure_rand', boolean)])
+class Shoe():
+  count: int
+  shoe: np.ndarray
+  pure_rand: bool
+
+  def __init__(self, count: int, shoe: np.ndarray, pure_rand: bool):
+    self.count = count
+    self.shoe = shoe
+    self.pure_rand = pure_rand
+
+  def sample_card(self):
+    if self.pure_rand:
+      return np.random.choice(cards_space)
+    card = self.shoe[self.count]
+    if self.count == 0:
+      self.count = self.shoe.shape[0]
+      self.shoe = self.shoe[np.random.permutation(self.count)]
+    self.count -= 1
+    return card
+
+@jit(nopython=True, cache=True)
+def get_sum(cards: np.ndarray, usable_ace: bool) -> int:
   has_ace = False
   cards_sum = 0
   for card in cards:
@@ -28,32 +48,30 @@ def get_sum(cards):
       has_ace == True
   if usable_ace and has_ace and cards_sum + 10 <= 21:
       return cards_sum + 10
-  else:
-    return cards_sum
+  return cards_sum
 
-def dealer_policy(dealer_cards, player_cards):
-  player_sum = get_sum(player_cards)
+@jit(nopython=True, cache=True)
+def dealer_policy(dealer_cards: np.ndarray, player_cards: np.ndarray, usable_ace: bool) -> int:
+  player_sum = get_sum(player_cards, usable_ace)
   if player_sum > 21:
     return stick_action
-  else:
-    dealer_sum = get_sum(dealer_cards)
-    if dealer_sum < 17:
-      return push_action
-    else:
-      return stick_action
+  dealer_sum = get_sum(dealer_cards, usable_ace)
+  if dealer_sum < 17:
+    return push_action
+  return stick_action
 
-def player_policy(dealer_cards, player_cards):
-  player_sum = get_sum(player_cards)
+@jit(nopython=True, cache=True)
+def player_policy(dealer_cards: np.ndarray, player_cards: np.ndarray, usable_ace: bool, A: np.ndarray) -> int:
+  player_sum = get_sum(player_cards, usable_ace)
   if player_sum <= 21:
-    dealer_showed_card = dealer_cards[1]
-    return A[player_sum - 12, dealer_showed_card - 1]
-  else:
-    return stick_action
+    return A[player_sum - 12, dealer_cards[1] - 1]
+  return stick_action
 
 
-def get_result(dealer_cards, player_cards):
-  player_sum = get_sum(player_cards)
-  dealer_sum = get_sum(dealer_cards)
+@jit(nopython=True, cache=True)
+def get_result(dealer_cards: np.ndarray, player_cards: np.ndarray, usable_ace: bool) -> int:
+  player_sum = get_sum(player_cards, usable_ace)
+  dealer_sum = get_sum(dealer_cards, usable_ace)
   if player_sum > 21:
     return -1
   elif dealer_sum > 21:
@@ -62,53 +80,76 @@ def get_result(dealer_cards, player_cards):
     return -1
   elif player_sum > dealer_sum:
     return 1
-  elif player_sum == dealer_sum:
-    return 0
+  return 0
 
-def value_update(dealer_cards, player_cards, result):
-  player_sum = get_sum(player_cards)
-  dealer_showed_card = dealer_cards[1]
+@jit(nopython=True, cache=True)
+def history_player_cards(player_cards: np.ndarray, i :int):
+  return player_cards[np.arange(i+2)]
+
+@jit(nopython=True, cache=True)
+def value_update(dealer_cards: np.ndarray, player_cards: np.ndarray, result: int, V: np.ndarray, nV: np.ndarray, usable_ace: bool) -> None:
+  player_sum = get_sum(player_cards, usable_ace)
   for i in range(len(player_cards)-1):
-    player_sum = get_sum([player_cards[j] for j in range(0, i+2)])
-    if player_sum <= 21:
-      n = nV[player_sum - 12, dealer_showed_card - 1]
-      V[player_sum - 12,  dealer_showed_card - 1] += (1./n) * (result - V[player_sum - 12, dealer_showed_card - 1])
-      nV[player_sum - 12, dealer_showed_card - 1] += 1
-
-
-def policy_eval():
-  player_cards, dealer_cards = [], []
-  i = 0
-  while True:
-    i += 1
-    prev_V = V.copy()
-    for _ in range(1000):
-      player_cards.append(sample_card())
-      dealer_cards.append(sample_card())
-      player_cards.append(sample_card())
-      dealer_cards.append(sample_card())
-
-      while get_sum(player_cards) < 12:
-        player_cards.append(sample_card())
-
-      while player_policy(dealer_cards, player_cards) == push_action:
-        player_cards.append(sample_card())
-
-      while dealer_policy(dealer_cards, player_cards) == push_action:
-        dealer_cards.append(sample_card())
-      result = get_result(dealer_cards, player_cards)
-
-      value_update(dealer_cards, player_cards, result)
-
-      player_cards.clear()
-      dealer_cards.clear()
-
-    delta = np.max(abs(V - prev_V))
-    print(i)
-    if delta < accuracy:
-      print("Evaluation finished!")
-      print(V)
-      print(nV)
+    if player_cards[i+1] == 0:
       break
+    player_sum = get_sum(history_player_cards(player_cards, i), usable_ace=usable_ace)
+    if player_sum <= 21 and player_sum >= 12:
+      ii = player_sum - 12
+      jj = dealer_cards[1] - 1
+      V[ii, jj] += (1.0 / nV[ii, jj]) * (result - V[ii, jj])
+      nV[ii,  jj] += 1
 
-policy_eval()
+@jit(nopython=True, cache=True)
+def append_card(cards: np.ndarray, i: int, card: int) -> int:
+  cards[i] = card
+  return i+1
+
+@jit(nopython=True, cache=True)
+def clear_cards(cards: np.ndarray) -> None:
+  cards.fill(0)
+
+
+@jit(nopython=True, cache=True)
+def policy_eval(
+    usable_ace: bool=False,
+    accuracy: float=1e-2,
+    pure_rand: bool=True) -> tuple[np.ndarray, np.ndarray]:
+  player_cards = np.zeros((21,), dtype=np.int8)
+  dealer_cards = np.zeros((21,), dtype=np.int8)
+  A = np.zeros((10, 10), dtype=np.int8)
+  V = np.zeros((10, 10), dtype=np.float32)
+  nV = np.ones((10, 10), dtype=np.int32)
+  shoe_buffer = np.repeat(cards_space, 4 * 8)
+  count = shoe_buffer.shape[0] - 1
+  shoe = Shoe(count, shoe_buffer, pure_rand)
+  A[:5, :] = 1
+  while True:
+    V_old = V.copy()
+    for _ in range(1000):
+      ii = 0
+      jj = 0
+      ii = append_card(player_cards, ii, shoe.sample_card())
+      jj = append_card(dealer_cards, jj, shoe.sample_card())
+      ii = append_card(player_cards, ii, shoe.sample_card())
+      jj = append_card(dealer_cards, jj, shoe.sample_card())
+
+      while get_sum(player_cards, usable_ace) < 12:
+        ii = append_card(player_cards, ii, shoe.sample_card())
+
+      while player_policy(dealer_cards, player_cards, usable_ace, A) == push_action:
+        ii = append_card(player_cards, ii, shoe.sample_card())
+
+      while dealer_policy(dealer_cards, player_cards, usable_ace) == push_action:
+        jj = append_card(dealer_cards, jj, shoe.sample_card())
+
+      result = get_result(dealer_cards, player_cards, usable_ace)
+
+      value_update(dealer_cards, player_cards, result, V, nV, usable_ace)
+
+      clear_cards(player_cards)
+      clear_cards(dealer_cards)
+    delta = np.max(np.abs(V - V_old))
+    if delta < accuracy:
+      break
+  print("Evaluation finished!")
+  return V, nV
