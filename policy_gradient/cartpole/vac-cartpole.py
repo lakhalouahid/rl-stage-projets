010c80e654f64fa65fa6cdd6861093121da9a48b
Ouahid LAK-HAL <ouahid.lak-hal@etu.uca.fr>
2022-04-30 00:47:05 +0200

    feat(algo): implement vanilla actor critic algo

--- policy_gradient/cartpole/vac-cartpole.py
+++ policy_gradient/cartpole/vac-cartpole.py
@@ -21,6 +21,26 @@ device  = torch.device("cpu")
 def discount_cumsum(x, discount):
   return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
 
+class ValueNetwork(nn.Module):
+  def __init__(self) -> None:
+    super().__init__()
+    self.ln1 = nn.Linear(state_size , hidden_size)
+    self.ln2 = nn.Linear(hidden_size, hidden_size)
+    self.ln3 = nn.Linear(hidden_size, action_size)
+    nn.init.xavier_uniform_(self.ln1.weight)
+    nn.init.zeros_(self.ln1.bias)
+    nn.init.xavier_uniform_(self.ln2.weight)
+    nn.init.zeros_(self.ln2.bias)
+    nn.init.xavier_uniform_(self.ln3.weight)
+    nn.init.zeros_(self.ln3.bias)
+
+  def forward(self, x):
+    x = torch.as_tensor(x, dtype=torch.float32).to(device)
+    x = torch.relu(self.ln1(x))
+    x = torch.relu(self.ln2(x))
+    x = self.ln3(x)
+    return x
+
 class PolicyNetwork(nn.Module):
   def __init__(self) -> None:
     super().__init__()
@@ -51,46 +71,67 @@ class PolicyNetwork(nn.Module):
 
 class Episode(object):
   def __init__(self):
-    self.s = []
+    self.s, self.a, self.lp, self.q = [], [], [], []
+    self.next_s, self.next_a, self.next_lp, self.next_q = [], [], [], []
     self.r = []
-    self.a = []
-    self.lp = []
 
-  def push(self, s, a, r, lp):
+  def push(self, s, a, lp, q, r, next_s, next_a, next_lp, next_q):
     self.s.append(s)
     self.a.append(a)
-    self.r.append(r)
+    self.q.append(q)
     self.lp.append(lp)
+    self.next_s.append(next_s)
+    self.next_a.append(next_a)
+    self.next_q.append(next_q)
+    self.next_lp.append(next_lp)
+    self.r.append(r)
 
   def finish_episode(self):
-    self.v = discount_cumsum(self.r, discount=discount)
     self.s_ = torch.tensor(np.array(self.s), dtype=torch.float32)
-    self.v_ = torch.tensor(np.copy(self.v), dtype=torch.float32)
     self.a_ = torch.tensor(self.a, dtype=torch.float32)
     self.lp_ = torch.stack(self.lp)
+    self.q_ = torch.stack(self.q)
+    self.next_s_ = torch.tensor(np.array(self.next_s), dtype=torch.float32)
+    self.next_a_ = torch.tensor(self.next_a, dtype=torch.float32)
+    self.next_lp_ = torch.stack(self.next_lp)
+    self.next_q_ = torch.stack(self.next_q)
+    self.r_ = torch.tensor(np.array(self.r), dtype=torch.float32)
 
 
 policy_net = PolicyNetwork()
-optimizer = torch.optim.Adam(policy_net.parameters(), lr=0.001)
+policy_opt = torch.optim.Adam(policy_net.parameters(), lr=0.001)
+
+value_net = ValueNetwork()
+value_opt = torch.optim.Adam(value_net.parameters(), lr=0.0001)
 
 def train(episode):
-  v = episode.v_
+  v = episode.q_.detach()
   lp = episode.lp_
   v = (v - torch.mean(v)) / torch.std(v)
-  policy_grad = -lp * v
+  policy_loss = -lp * v
   policy_net.zero_grad()
-  policy_grad.mean().backward()
-  optimizer.step()
+  policy_loss.mean().backward()
+  policy_opt.step()
 
+  value_loss = (episode.r_ + discount * episode.next_q_.detach() - episode.q_)**2
+  value_net.zero_grad()
+  value_loss.mean().backward()
+  value_opt.step()
 try:
   for i in range(num_episodes):
-    s = env.reset()
+    next_s = env.reset()
+    next_a, next_lp = policy_net(next_s[None, ...])
+    next_q = value_net(next_s[None, ...])[0][next_a]
     episode = Episode()
     for j in range(max_episode_len):
       # env.render()
-      a, lp = policy_net(s[None, ...])
+      a, lp = next_a, next_lp
+      s = next_s
+      q = next_q
       next_s, r, d, _ = env.step(a)
-      episode.push(s, a, r, lp)
+      next_a, next_lp = policy_net(next_s[None, ...])
+      next_q = value_net(next_s[None, ...])[0][next_a]
+      episode.push(s, a, lp, q, r, next_s, next_a, next_lp, next_q)
       s = next_s
       if d:
         episode.finish_episode()
