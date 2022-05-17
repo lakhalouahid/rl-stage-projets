3db8bfc660ca4f5e68e3545e5e21cb24dbd59a20
Ouahid LAK-HAL <ouahid.lak-hal@etu.uca.fr>
2022-05-17 20:42:36 +0200

    feat(test): test the package debugnn

--- representation-learning/grid-world/autoencoder-agent.py
+++ representation-learning/grid-world/autoencoder-agent.py
@@ -1,5 +1,4 @@
 import os
-import pdb
 import argparse
 import logging
 import torch
@@ -9,15 +8,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-from time import sleep, time
 from torch import nn
 
 batchsize=25
 parser = argparse.ArgumentParser()
 parser.add_argument("--dictstate")
 parser.add_argument("--test", action="store_true")
-parser.add_argument("--lambda", type=float, default=1.0)
-parser.add_argument("--exponent", type=float, default=1.0)
+parser.add_argument("--plain", action="store_true")
+parser.add_argument("--lbd", type=float, default=1.0)
 parser.add_argument("--maxlr", type=float, default=5e-3)
 parser.add_argument("--minlr", type=float, default=5e-4)
 parser.add_argument("--lrdecay", type=float, default=0.9999)
@@ -25,22 +23,18 @@ parser.add_argument("--weightdecay", type=float, default=0.9995)
 parser.add_argument("--loop", type=int, default=250000)
 args = parser.parse_args()
 device = torch.device("cuda:0")
-logfile = f"logs/autoencoder-{'plain' if args.plain else 'simple'}-{time():.0f}.log"
+root = os.getcwd()
+logfile = os.path.join(root, "logs/train.log")
+
+os.mkdir(os.path.join(root, "logs"))
+os.mkdir(os.path.join(root, "checkpoints"))
+os.mkdir(os.path.join(root, "data"))
 if not args.test:
   logging.basicConfig(filename=logfile, format="%(message)s", level=logging.INFO)
 
+
 w, h, n = 80, 80, 5
 a, b = w//n, h//n
-
-mean_rewards = torch.zeros((2, n, n), dtype=torch.float32).to(device)
-mean_rewards_count = torch.ones((2, n, n), dtype=torch.float32).to(device)
-
-def get_b(poses, k):
-  return mean_rewards[k][poses]
-
-def update_b(poses, rewards, k):
-  mean_rewards[k][poses] -= (mean_rewards[k][poses] - rewards[:, 0]) / mean_rewards_count[k][poses]
-  mean_rewards_count[k][poses] += 1.0
 
 bg_color = (255, 255, 255)
 action_space = (0, 1, 2, 3)
@@ -65,24 +59,6 @@ class GW():
         rect = pygame.rect.Rect((center[0] - a//2, center[1] - b//2), (a, b))
         pygame.draw.rect(self.screen, bg_color, rect)
 
-  def visualize(self):
-    f, axes = plt.subplots(n, n)
-    for i in range(n):
-      for j in range(n):
-        axes[i][j].imshow(self.frames[i, j].moveaxis(0, 2).cpu().numpy())
-    plt.show(block=False)
-
-  @staticmethod
-  def visualize_frames(frames, size):
-    f, axes = plt.subplots(size[0], size[1])
-    k = 0
-    for i, j in zip(*[shit.flat for shit in np.mgrid[0:size[0], 0:size[1]]]):
-      axes[i][j].imshow(frames[k].moveaxis(0, 2).cpu().numpy())
-      k += 1
-      if k == len(frames):
-        break
-    plt.show(block=False)
-
   def get_torch_frame(self):
     frame = pygame.surfarray.pixels3d(self.screen)
     return GW.to_float_tensor(frame[:, :, :1])
@@ -97,12 +73,10 @@ class GW():
   def compute_center(idx_a, idx_b):
     return (idx_a * a + a//2, idx_b * b + b//2)
 
-
   def sample(self):
     sampled_flat_indexes = np.random.permutation(n*n)
     poses = np.unravel_index(sampled_flat_indexes, shape=(n, n))
     frames = self.frames[poses]
-    # GW.visualize_frames(frames)
     return frames, poses
 
   def step(self, poses, actions):
@@ -110,7 +84,6 @@ class GW():
     actions = (actions[:, 0], actions[:, 1])
     next_poses = tuple([(_poses + _actions + n) % n for _poses, _actions in zip(poses, actions)])
     frames = self.frames[next_poses]
-    # GW.visualize_frames(frames)
     return frames, poses
 
 
@@ -217,7 +190,7 @@ class FeaturePolicy(nn.Module):
   def __init__(self):
     super(FeaturePolicy, self).__init__()
     self.fc = nn.Sequential(
-        nn.Linear(2, 4),
+      nn.Linear(2, 4),
     )
 
   def forward(self, x):
@@ -225,16 +198,14 @@ class FeaturePolicy(nn.Module):
     x = torch.softmax(x, dim=1)
     x = (x + 0.01) / (1 + 0.01*4)
     return x
-minlr=1e-5
-maxlr=1e-4
-lbd=args.lbd
+
 vanilla_autoencoder = VanillaAutoEncoder().to(device)
-ae_optimizer = torch.optim.Adam(params=vanilla_autoencoder.parameters(), lr=maxlr, weight_decay=0.2)
-ae_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=ae_optimizer, gamma=args.decay)
+ae_optimizer = torch.optim.Adam(params=vanilla_autoencoder.parameters(), lr=args.maxlr, weight_decay=args.weightdecay)
+ae_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=ae_optimizer, gamma=args.lrdecay)
 
 policies = [FeaturePolicy().to(device) for _ in range(2)]
-pc_optimizers = [torch.optim.Adam(params=policy.parameters(), lr=maxlr, weight_decay=0.2) for policy in policies]
-pc_schedulers = [torch.optim.lr_scheduler.ExponentialLR(optimizer=pc_optimizer, gamma=args.decay) for pc_optimizer in pc_optimizers]
+pc_optimizers = [torch.optim.Adam(params=policy.parameters(), lr=args.maxlr, weight_decay=args.weightdecay) for policy in policies]
+pc_schedulers = [torch.optim.lr_scheduler.ExponentialLR(optimizer=pc_optimizer, gamma=args.lrdecay) for pc_optimizer in pc_optimizers]
 
 def init_grid_world():
   grid_world = GW()
@@ -266,11 +237,13 @@ grid_world = init_grid_world()
 rlossref = 1800
 
 def train():
-  for i in range(args.repeat):
+  for i in range(args.loop):
     vanilla_autoencoder.train()
     states, poses = grid_world.sample()
     rstates, _latents = vanilla_autoencoder.forward(states)
     rloss = torch.sum(torch.sum(0.5 * torch.square(rstates - states), dim=1))
+    if torch.isnan(rloss):
+      break
     rloss.backward()
     if args.plain:
       ae_optimizer.step()
@@ -289,7 +262,7 @@ def train():
         next_latents = vanilla_autoencoder.encode(next_states.to(device))
         dlatents = torch.abs(next_latents - latents.detach())
         rewards  = dlatents[:, k:k+1] / torch.sum(dlatents, dim=1, keepdim=True)
-        sloss[k] = torch.sum(-lprob_actions * rewards ) * lbd
+        sloss[k] = torch.sum(-lprob_actions * rewards ) * args.lbd
         sloss[k].backward()
         if i % 100 == 0:
           print(f"policy loss {k}: {sloss[k]:.3f}")
@@ -299,7 +272,7 @@ def train():
       pc_optimizers[0].zero_grad()
       pc_optimizers[1].zero_grad()
       ae_optimizer.zero_grad()
-      if ae_scheduler.get_lr()[0] > minlr:
+      if ae_scheduler.get_lr()[0] > args.minlr:
         ae_scheduler.step()
         pc_optimizers[0].step()
         pc_optimizers[1].step()
@@ -309,53 +282,9 @@ def train():
       print("---------------------------------------------------------")
 
     if i % 10000 == 0 :
-      filename = os.path.join("checkpoints", f"{'plain' if args.plain else 'simple'}-ae-{time():.0f}-{rloss}-{lbd}-{args.exp:0f}-{args.decay}.pt")
+      filename = os.path.join("checkpoints", f"{rloss}.pt")
       torch.save(vanilla_autoencoder.state_dict(), filename)
 
 
-def visualise_latents(latents):
-  f, axe = plt.subplots()
-  X = latents[:, 0]
-  Y = latents[:, 1]
-  axe.scatter(X, Y, s=100, color="red")
-  axe.set_xlabel("X")
-  axe.set_ylabel("Y")
-  for i in range(batchsize):
-    idx = np.unravel_index(i, (n, n))
-    axe.text(X[i], Y[i], f"({idx[0]}, {idx[1]})")
-  plt.show(block=False)
-
-def visualise_latents2(latents):
-  f, axe = plt.subplots()
-  X = latents[:, 0]
-  Y = latents[:, 1]
-  Xi = np.arange(5)
-  Yi = np.arange(5)
-  axe.scatter(Xi, Yi, s=100, color="red")
-  axe.set_xlabel("X")
-  axe.set_ylabel("Y")
-  for i in range(batchsize):
-    idx = np.unravel_index(i, (n, n))
-    print(idx)
-    axe.text(Xi[idx[0]], Yi[idx[1]], f"({X[i]:.2f}, {Y[i]:.2f})")
-  plt.show(block=False)
-
-def test():
-  vanilla_autoencoder.load_state_dict(torch.load("checkpoints/" + args.file))
-  frames = grid_world.frames.flatten(start_dim=0, end_dim=1)
-  rframes, latents = vanilla_autoencoder(frames)
-  grid_world.visualize_frames(frames.detach().cpu(), (n, n))
-  grid_world.visualize_frames(rframes.detach().cpu(), (n, n))
-  print(latents)
-  np_latents = latents.detach().cpu().numpy()
-  visualise_latents(np_latents)
-  visualise_latents2(np_latents)
-  input()
-
-
-
 if __name__ == '__main__':
-  if args.test:
-    test()
-  else:
-    train()
+  train()
---
+++ representation-learning/grid-world/debugnn_config.py
@@ -0,0 +1,3 @@
+import debugnn
+
+debugnn.train()
